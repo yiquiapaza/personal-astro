e8ff4f7c3941ad48fa5a4f90da813ada97ff4da0
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-11-23 17:44:02 -0300

    feat: add tailwind and config project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), 
+    tailwind({
+      config: { path: './tailwind.config.cjs' }
+    }), image()]
 });
